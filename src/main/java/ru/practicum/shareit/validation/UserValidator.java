e7d47a47d42b5ca3849bbf7f0c261328919a1eed
Petr <red19650226@yandex.ru>
2025-12-19 02:06:50 +0300

    feat: update UserValidator

--- src/main/java/ru/practicum/shareit/validation/UserValidator.java
+++ src/main/java/ru/practicum/shareit/validation/UserValidator.java
@@ -2,11 +2,17 @@ package ru.practicum.shareit.validation;
 
 import java.util.Set;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import ru.practicum.shareit.exception.EmailAlreadyExistsException;
+import ru.practicum.shareit.exception.NotFoundException;
+import ru.practicum.shareit.user.repository.UserRepository;
 
 @Component
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
 public class UserValidator {
+    private final UserRepository userRepository;
 
     public void validateUniqueEmail(String email, Set<String> emailSet) {
         if (!isUniqueEmail(email, emailSet)) {
@@ -18,4 +24,13 @@ public class UserValidator {
         return emailSet.add(email);
     }
 
+    public void validateExistingUser(long userId) {
+        if (isExistingUser(userId)) {
+            throw new NotFoundException(String.format("Пользователь с ID %d не найден", userId));
+        }
+    }
+
+    private boolean isExistingUser(long userId) {
+        return userRepository.get(userId).isEmpty();
+    }
 }
