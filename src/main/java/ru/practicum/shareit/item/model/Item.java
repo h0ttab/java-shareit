6456459bccd056667106a4f7b7cb30fc40109dc7
Petr <red19650226@yandex.ru>
2025-12-19 02:11:15 +0300

    feat: update Item model

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,15 +1,16 @@
 package ru.practicum.shareit.item.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import ru.practicum.shareit.request.model.ItemRequest;
-import ru.practicum.shareit.user.model.User;
 
 @Data
+@AllArgsConstructor
 public class Item {
     private final long id;
     private String name;
     private String description;
     private boolean available;
-    private final User owner;
+    private final long ownerId;
     private final ItemRequest request;
 }
